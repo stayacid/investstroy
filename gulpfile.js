921f08e6f8ad792778bcca1247c7d1937a545d6c
stayacid <stayacid@gmail.com>
2018-05-29 15:35:52 +0300

    trying to fix gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@
 
 // Подключение плагинов через переменные (connection of plugins through variables):
 var gulp = require('gulp'), // Gulp
-    debug = require('gulp-debug'), // Отслеживание работы тасков в терминале (operation tracking of tasks in terminal)
     autoprefixer = require('gulp-autoprefixer'), // Добавление вендорных префиксов (adding of vendor prefixers)
     concat = require('gulp-concat'), // Объединение файлов (files merger)
     csso = require('gulp-csso'), // Минификация CSS-файлов (minification of CSS files)
@@ -12,10 +11,7 @@ var gulp = require('gulp'), // Gulp
     pngquant = require('imagemin-pngquant'), // Оптимизация PNG-изображений (PNG images optimization)
     rename = require('gulp-rename'), // Переименование файлов (files rename)
     sass = require('gulp-sass'), // sass
-    uglify = require('gulp-uglify'), // Минификация JS-файлов (minification of JS files)
-    sftp = require('gulp-sftp'),
-    ftp = require('vinyl-ftp'),
-    gutil = require('gulp-util'); 
+    uglify = require('gulp-uglify'); // Минификация JS-файлов (minification of JS files)
 
 // Задание путей к используемым файлам и папкам (paths to folders and files):
 var paths = {
@@ -24,18 +20,20 @@ var paths = {
     dist: './dist'
   },
   watch: {
-    html: './app//*.html',// Путь для вотчера html-файлов (path for watcher to html files)
-    sass: './app/sass/**/*.sass',// Путь для вотчера Sass-файлов (path for watcher to Sass files)
-    js: './app/js/*.js'
+    pug: './app/*.html', //для browserSync
+    sass: './app/sass/*.sass',
+    js: './app/js/**/*.js'
   },
   app: {
-    html: {
-      src: './app/index.html',
+    html: { 
+      src: './app/pug/pages/*.pug', //для таска преобразования pug в html
       dest: './app'
     },
-    common: {
+    common: { //для тасков минификации файлов
       css: {
-        src: './app/sass/main.sass',
+        src: [
+          './app/sass/**/*.sass'
+        ],
         dest: './app/css'
       },
       js: {
@@ -47,23 +45,12 @@ var paths = {
       css: {
         src: [
           './app/libs/bootstrap-grid/bootstrap-grid.min.css'
-          /*'./app/libs/fontawesome/font-awesome.min.css',
-          './app/libs/mmenu/css/jquery.mmenu.all.css',
-          './app/libs/css-hamburgers/hamburgers.css',
-          './app/libs/owl.carousel/dist/assets/owl.carousel.min.css',
-          './app/libs/fotorama/fotorama.css',
-          './app/libs/selectize/css/selectize.css'*/
         ],
         dest: './app/css'
       },
       js: {
         src: [
           './app/libs/jquery/jquery-3.3.1.slim.min.js'
-          /*'./app/libs/mmenu/js/jquery.mmenu.all.min.js',
-          './app/libs/owl.carousel/dist/owl.carousel.min.js',
-          './app/libs/equalHeights/equalheights.js',
-          './app/libs/fotorama/fotorama.js',
-          './app/libs/selectize/js/standalone/selectize.min.js'*/
         ],
         dest: './app/js/min'
       },
@@ -71,7 +58,7 @@ var paths = {
         src: [
           './app/fonts/*.*'
         ],
-        dest: './app/fonts'
+        dest: './app/assets/fonts'
       }
     }
   },
@@ -89,11 +76,11 @@ var paths = {
       dest: './dist/css'
     },
     js: {
-      src: './app/js/min/*.min.js',
+      src: './app/js/*.min.js',
       dest: './dist/js'
     },
     fonts: {
-      src: './app/fonts/**/*.*',
+      src: './app/fonts/*.*',
       dest: './dist/fonts'
     }
   }
@@ -105,52 +92,36 @@ var browserSync = require('browser-sync').create(),
 
 // Таск для работы Browsersync, автообновление браузера (Browsersync task, autoreload of browser):
 gulp.task('serve', function() {
-/*   browserSync.init({
-    server: './app'
-  }); */
   browserSync.init({
-    server: { // Настройки сервера (server settings)
-      baseDir: paths.dir.app, // Базовая директория (basic directory)
-      index: 'index.html' // Индексный файл (index file)
-    }
+    server: './app'
   });
-/*   gulp.watch(paths.watch.pug, gulp.series('html'));
+  /* gulp.watch(paths.watch.pug, gulp.series('html')); */
+  gulp.watch('app/*.html', browserSync.reload); // Отслеживание изменений в html
   gulp.watch(paths.watch.sass, gulp.series('cssCommon'));
-  gulp.watch(paths.watch.js, gulp.series('jsCommon')); */
-  gulp.watch([paths.watch.html, paths.watch.sass, paths.watch.js], gulp.series('build')); // Отслеживание изменений Pug и Sass-файлов (change tracking of Pug and Sass files)
-  gulp.watch('*.html').on('change', reload);
+  gulp.watch(paths.watch.js, gulp.series('jsCommon'));
 });
 
 // Таск для работы Pug, преобразование Pug в HTML (Pug to HTML conversion task):
-/*gulp.task('html', function() {
-  return gulp.src(paths.app.html.src) // Исходник таска html (source of html task)
-    .pipe(plumber()) // Обработка ошибок таска html (error handling of html task) 
-    .pipe(debug({title: 'Pug source'})) // Отслеживание исходника таска html (source tracking of html task)
-    .pipe(pug({
-      pretty: true, // Форматирование разметки в HTML-файле (code formatting in HTML file)
-      doctype: 'HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"' // Установка doctype (setting of doctype)
-    }))
-    .pipe(debug({title: 'Pug'})) // Отслеживание работы плагина Pug (operation tracking of Pug plugin)
-    .pipe(gulp.dest(paths.app.html.dest)) // Сохранение HTML-шаблона письма в папке app (save of HTML template in folder app)
-    .pipe(debug({title: 'Pug dest'})) // Отслеживание сохранения HTML-шаблона (saving tracking of HTML template)
-    .pipe(browserSync.stream()); // Browsersync
-});*/
+/* gulp.task('html', function () {
+  return gulp.src(paths.app.html.src)
+    .pipe(plumber())
+    .pipe(pug({pretty: true}))
+    .pipe(gulp.dest(paths.app.html.dest))
+    .pipe(browserSync.stream());
+}); */
 
 // Таск для преобразования sass-файлов в CSS (sass to CSS conversion):
 gulp.task('cssCommon', function() {
   return gulp.src(paths.app.common.css.src) // Исходник таска css (css task source)
     .pipe(plumber()) // Обработка ошибок таска css (error handling of css task)
-    .pipe(debug({title: 'Sass source'})) // Отслеживание исходника таска css (source tracking of css task)
     .pipe(concat('main.sass'))
     .pipe(sass().on('error', sass.logError)) // Преобразование Sass в CSS (Sass to CSS conversion)
-    .pipe(debug({title: 'Sass'})) // Отслеживание работы плагина Sass (operation tracking of Sass plugin)
     .pipe(autoprefixer(['last 15 versions', '> 1%'], { cascade: true}))
     .pipe(gulp.dest(paths.app.common.css.dest)) // Сохранение CSS-файлов в папке app/css (saving of CSS files in folder app/css)
     .pipe(rename({suffix: '.min'}))
     .pipe(csso())
     .pipe(gulp.dest(paths.app.common.css.dest)) // Сохранение CSS-файлов в папке app/css (saving of CSS files in folder app/css)
-    .pipe(debug({title: 'Sass dest'})) // Отслеживание сохранения (saving tracking)
-    .pipe(browserSync.reload({stream: true})) // Browsersync
+    .pipe(browserSync.stream()); // Browsersync
 });
 
 // Таск для объединения и минификации пользовательских JS-файлов (task for merger and minification custom JS files)
@@ -158,6 +129,7 @@ gulp.task('jsCommon', function() {
   return gulp.src(paths.app.common.js.src)
     .pipe(plumber())
     .pipe(concat('common.js'))
+    .pipe(gulp.dest(paths.app.common.js.dest))
     .pipe(rename({suffix: '.min'}))
     .pipe(uglify())
     .pipe(gulp.dest(paths.app.common.js.dest))
@@ -181,10 +153,10 @@ gulp.task('jsVendor', function () {
 });
 
 // Таск для объединения папок fonts внешних библиотек (task for merger fonts folders of libraries, plugins and frameworks)
-gulp.task('fontsVendor', function () {
+/* gulp.task('fontsVendor', function () {
   return gulp.src(paths.app.vendor.fonts.src)
     .pipe(gulp.dest(paths.app.vendor.fonts.dest));
-});
+}); */
 
 // Таск для предварительной очистки (удаления) production-папки (task for delete of production folder dist):
 gulp.task('clean', function() {
@@ -211,26 +183,8 @@ gulp.task('dist', function () {
   return htmlDist, cssDist, jsDist, fontsDist;
 });
 
-gulp.task('deploy', function() {
-
-	var conn = ftp.create({
-		host:      's29.webhost1.ru',
-		user:      'login',
-		password:  'pass',
-		parallel:  10,
-		log: gutil.log
-	});
-
-	var globs = [
-	'dist/**'
-	];
-	return gulp.src(globs, {buffer: false})
-	.pipe(conn.dest('/ivanzagainov/subs/smitler'));
-
-});
-
 // Таск для сборки (build task):
-gulp.task('build', gulp.parallel('cssCommon', 'jsCommon', 'cssVendor', 'jsVendor', 'fontsVendor'));
+gulp.task('build', gulp.parallel(/* 'html', */ 'cssCommon', 'jsCommon', 'cssVendor', 'jsVendor'/* , 'fontsVendor' */));
 
 // Таск для разработки (development task):
 gulp.task('default', gulp.series('build', 'serve'));
